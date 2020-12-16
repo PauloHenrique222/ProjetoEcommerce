2c9d5eb01c13a9aa4c94230eb8594659b3c02d1c
paulohenrique222 <pauloti4321@gmail.com>
2020-12-15 21:29:00 -0300

    Gerenciamento de erros

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -8,8 +8,15 @@ class Backoffice::SendMailController < ApplicationController
   end
 
   def create
-    AdminMailer.send_email(current_admin, params[:'recipient-name'],
+    begin
+      AdminMailer.send_email(current_admin, params[:'recipient-name'],
                            params[:'subject-text'], params[:'message-text']).deliver_now
+      @notify_message = "Email successfully sent"
+      @notify_flag ="success"
+    rescue
+      @notify_message = "Error sending the email"
+      @notify_flag="error"
+    end
     respond_to do |format|
       format.js
     end
